82c9b886d2b74f6fca4c2a1d55c30cfd8af769b5
henok yaz <h_yazie@yahoo.com>
2020-05-20 21:13:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,45 +1,31 @@
-def load_library
-  # code goes here
-def load_library(yaml_file)
-  response = {"get_meaning" => {}, "get_emoticon" => {}}
-  library = YAML.load_file(yaml_file)
-  library.each do |trans, emos|
-    response["get_meaning"][emos[1]] = trans
-    response["get_emoticon"][emos[0]] = emos[1]
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  emoticon_hash = Hash.new
+
+  emoticon_hash["get_emoticon"] = Hash.new
+  emoticon_hash["get_meaning"] = Hash.new
+
+  emoticons.each do |english_word, emoticon_set|
+    emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
+    emoticon_hash["get_meaning"][emoticon_set.last] = english_word
   end
-  response
+  emoticon_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
-def get_japanese_emoticon(yaml_file, emoticon)
-  library = load_library(yaml_file)
-  response = nil
-  library["get_emoticon"].each do |english, japanese|
-    if emoticon == english
-      response = japanese
-    end
+def get_japanese_emoticon(path, emoticon)
+  emoticon_hash = load_library(path)
+  result = emoticon_hash["get_emoticon"][emoticon]
+  if result == nil
+    result = "Sorry, that emoticon was not found" 
   end
-  if response == nil
-    return "Sorry, that emoticon was not found"
-  else
-    return response
-  end
+  result
 end
 
-def get_english_meaning
-  # code goes here
-def get_english_meaning(yaml_file, emoticon)
-  library = load_library(yaml_file)
-  response = nil
-  library["get_meaning"].each do |emo, trans|
-    if emo == emoticon
-      response = trans
-    end
+def get_english_meaning(path, emoticon)
+  emoticon_hash = load_library(path)
+  result = emoticon_hash["get_meaning"][emoticon]
+  if result == nil
+    result = "Sorry, that emoticon was not found" 
   end
-  if response == nil
-    return "Sorry, that emoticon was not found"
-  else
-    return response
-  end
-end 
+  result
+end
